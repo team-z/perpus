8821988a7753c54071c8e23e3b4ebe03f91bd35e
DESKTOP-DVCUVBK\dika <mahardikaari51@yahoo.com>
2017-01-26 11:33:09 +0700

    Halaman Login Admin , Silahkan di pull #Dika

---
+++ admin/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=utf-8>
+    <meta name=description content="">
+    <meta name=viewport content="width=device-width, initial-scale=1">
+	<title>Admin Login</title>
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+	<style type="text/css">
+		.jumbotron{background-color: rgba(255,255,255,0);font-family: 'Raleway',sans-serif;color: white;}
+	</style>
+</head>
+<body style="background: url('../img/admin-login.jpg');background-size: cover;">
+<br><br><br><br><br><br>
+   <div class="container">
+     <div class="row">
+        <div class="col-md-8">
+   	        <div class="jumbotron">
+   		       <h1>Selamat Datang Di Login Admin</h1>
+   		       <p>Untuk mengakses halaman admin silahkan masukkan user dan password anda , belum tergabung ? silahkan tanyakan pada admin untuk didaftarkan</p>
+   	        </div>
+         </div>
+
+         <div class="col-md-4">
+           <div class="jumbotron" style="background-color: rgba(255,255,255,0.4);">
+           <h1>Login</h1>
+         	 <form class="form-horizontal">
+               <div class="form-group">
+                <input type="text" class="form-control input-lg" placeholder="Username" name="">
+               </div>
+
+               <div class="form-group">
+                <input type="password" class="form-control input-lg" placeholder="Password" name="">
+               </div>
+
+               <div class="form-group">
+                <input type="submit" class="btn btn-block btn-lg btn-default" value="Login" name="">
+               </div>
+         	 </form>
+           </div>
+         </div>
+     </div>
+   </div>
+</body>
+</html>
